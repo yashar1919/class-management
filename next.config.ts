553859442a6ad481b478a5f3acc0b11464caeabd
YasharTalebi19 <yashartalebi19@gmail.com>
2025-09-14 20:16:59 +0330

    Add PWA✅

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+/* import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
@@ -9,4 +9,42 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default nextConfig; */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
+
+/* import withPWA from "next-pwa";
+import type { NextConfig } from "next";
+
+const isDev = process.env.NODE_ENV === "development";
+
+const baseConfig: NextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+const nextConfig = withPWA({
+  ...baseConfig,
+  pwa: {
+    dest: "public",
+    disable: isDev, // ⛔ غیرفعال کردن PWA در حالت dev
+    register: true,
+    skipWaiting: true,
+  },
+});
+
+export default nextConfig; */
