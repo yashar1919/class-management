b34adb8d87c3e9e95f2745c41bdce6eb9f87a2e3
YasharTalebi19 <yashartalebi19@gmail.com>
2025-11-01 09:47:30 +0330

    Fix deployment issues - clean config files

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,6 @@
-/* import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
-export default nextConfig; */
-
-//eslint-disable-next-line
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-  sw: "sw.js",
-  swSrc: "public/sw.js",
-});
-
-module.exports = withPWA({
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
@@ -50,31 +29,6 @@ module.exports = withPWA({
       },
     ];
   },
-});
-
-/* import withPWA from "next-pwa";
-import type { NextConfig } from "next";
-
-const isDev = process.env.NODE_ENV === "development";
-
-const baseConfig: NextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
-const nextConfig = withPWA({
-  ...baseConfig,
-  pwa: {
-    dest: "public",
-    disable: isDev, // ⛔ غیرفعال کردن PWA در حالت dev
-    register: true,
-    skipWaiting: true,
-  },
-});
-
-export default nextConfig; */
+export default nextConfig;
